550ab8f0806284d6026a50622a38ef983ede6462
rbo13 <chardyy.orofeo@gmail.com>
2023-09-27 21:19:08 +0800

    added Roles schema and used Hikari to pool PG connection

--- api/hacktoberfest-pictureboxd/src/main/resources/db/migration/V1__initial_db_migration.sql
+++ api/hacktoberfest-pictureboxd/src/main/resources/db/migration/V1__initial_db_migration.sql
@@ -1,9 +1,12 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE TYPE ROLES AS ENUM ('SUPER_USER', 'ADMIN', 'USER');
+
 CREATE TABLE IF NOT EXISTS "users" (
   "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   "email_address" VARCHAR(255) UNIQUE NOT NULL,
   "password" TEXT NOT NULL,
+  "role" ROLES NOT NULL DEFAULT 'USER',
   "created_at" TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
 
